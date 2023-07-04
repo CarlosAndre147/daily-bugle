a6a893bf607b669446aab247bea28affd9da899d
Kazandre <102616045+Kazandre@users.noreply.github.com>
2023-07-04 11:04:48 -0300

    change variable

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const password = process.env.PASSWORD;
 
 const connectDB = async () => {
   try {
-       const conn = await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
-       console.log('MongoDB Connected.');
+      const conn = await mongoose.connect(process.env.MONGO_URI);
+      console.log('MongoDB Connected.');
   } catch (error) {
       console.log(error);
       process.exit(1)
